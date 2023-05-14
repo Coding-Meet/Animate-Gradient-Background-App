5e5de3cc1db18d70838251149d414c59daf351af
coding meet <codingmeet26@gmail.com>
2023-05-14 21:23:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/animategradientbackgroundapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.animategradientbackgroundapp
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rootView = findViewById<ConstraintLayout>(R.id.rootView)
+
+        val drawable : AnimationDrawable = rootView.background as AnimationDrawable
+        drawable.setEnterFadeDuration(1500)
+        drawable.setExitFadeDuration(2000)
+        drawable.start()
+    }
+}
